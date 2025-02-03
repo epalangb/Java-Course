c1a5656cb6dab260c664a6ddb5855c8294fcb001
epalangb <epalangbod@gmail.com>
2025-02-03 15:39:47 +0100

    Added solution to Exercise4_1

--- Chapter-04/Exercise4_1.java
+++ Chapter-04/Exercise4_1.java
@@ -1,9 +1,23 @@
 
 public class Exercise4_1 {
-
+	/*
+	 * Print all the arguments you receive on the screen. If an argument has fewer
+	 * than five characters, write it in uppercase. Otherwise, write it in
+	 * lowercase.
+	 */
+	private static final int FEWER = 5;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (String argument : args) {
+			System.out.println(argsLong(argument));
+		}
 
 	}
-
+	private static String argsLong(String arg) {
+		if (arg.length() < FEWER) {
+			return arg.toUpperCase();
+		}else {
+			return arg.toLowerCase();
+		}
+	}
 }
